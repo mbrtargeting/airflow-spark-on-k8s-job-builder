2b7cd74acfe660ec3f713cac6fd9e9499f075e5b
Diogo Aurelio <diogo@mbr-targeting.com>
2025-01-29 16:25:30 +0100

    build(python): update requirements to fit airflow 2.10.3
    Signed-off-by: Diogo Aurelio <diogo@mbr-targeting.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airflow-spark-on-k8s-job-builder',
-    version='0.1.1',
+    version='0.1.5',
     author='Stroeer Labs Data Engineering',
     author_email='diogo.aurelio@stroeer.de',
     description='A library to limit the amount of unnecessary boilerplate code required when launching Spark Jobs against k8s Spark-Operator in Airflow',
