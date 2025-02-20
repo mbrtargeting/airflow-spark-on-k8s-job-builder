f3312dd09773493a5e234c32a57c42daf4fb0a3d
Diogo Aurelio <diogo@mbr-targeting.com>
2025-02-20 17:51:55 +0100

    refactor(app): move customizable spark operator into a dedicated file
    Signed-off-by: Diogo Aurelio <diogo@mbr-targeting.com>

--- airflow_spark_on_k8s_job_builder/core.py
+++ airflow_spark_on_k8s_job_builder/core.py
@@ -7,129 +7,18 @@
 import copy
 import logging
 from datetime import timedelta
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from airflow import DAG
 from airflow.models import BaseOperator
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
-    SparkKubernetesOperator,
-)
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
-    SparkKubernetesSensor,
-)
-from airflow.utils.context import Context
-from jinja2 import Template
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
 from airflow_spark_on_k8s_job_builder.constants import (
     DEFAULT_SPARK_VERSION, DEFAULT_NAMESPACE, SPARK_JOB_SPEC_TEMPLATE, OVERRIDE_ME,
 )
+from airflow_spark_on_k8s_job_builder.customizable_spark_k8s_operator import CustomizableSparkKubernetesOperator
 
 SPARK_AIRFLOW_TASK_GROUP = "spark_task_group"
-
-
-class CustomizableSparkKubernetesOperator(SparkKubernetesOperator):
-    """
-    A decorator that allows using airflow macros inside spark k8s template
-    It does so by intercepting execute method with a sole purpose of rendering
-    a second time the jinja values of the SparkApplication yaml manifest
-
-    In case you need to add another macros to be jinja rendered in the
-    SparkApplication manifest - for example ts_nodash_with_tz - just follow the next steps:
-        a) add another constructor variable
-
-            ```python
-                def __init__(
-                        self,
-                        *,
-                        application_file: str,
-                        template_field_ds: str,
-                        template_field_ts: str,
-                        template_field_ts_nodash_with_tz: str
-                        **kwargs,
-                ):
-                self.template_field_YOUR_NEW_VAR = template_field_YOUR_NEW_VAR
-            ```
-
-        b) add that variable to the template fields (before this class constructor):
-
-            ```python
-                template_fields: Sequence[str] = ("application_file",
-                                                  "template_field_ds",
-                                                  "template_field_ts",
-                                                  "template_field_ts_nodash_with_tz")
-            ```
-
-
-        c) inject those macros when the builder class creates
-
-
-            ```python
-                task = CustomizableSparkKubernetesOperator(
-                    task_id=self._task_id,
-                    params=self._job_spec['params'],
-                    dag=self._dag,
-                    namespace=self._namespace,
-                    application_file=self._application_file,
-                    retries=self._retries,
-                    do_xcom_push=True,
-                    execution_timeout=self._task_timeout,
-                    template_field_ds='{{ ds }}',
-                    template_field_ts='{{ ts }}',
-                    template_field_ts_nodash_with_tz='{{ ts_nodash_with_tz }}'
-                )
-            ```
-
-        d) in `execute` method, add that variable to template_context:
-
-            ```python
-
-                def execute(self, context: Context):
-                    template = Template(self.application_file)
-                    template_context = {
-                        "ds": self.template_field_ds,
-                        "ts": self.template_field_ts,
-                        "ts_nodash_with_tz": self.template_field_ts_nodash_with_tz,
-                    }
-            ```
-
-
-    Ref docs:
-        - Airflow macros: https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
-
-    """
-
-    template_fields: Sequence[str] = (
-        "application_file",
-        "template_field_ds",
-        "template_field_ts",
-        "data_interval_end",
-    )
-
-    def __init__(
-            self,
-            *,
-            application_file: str,
-            template_field_ds: str,
-            template_field_ts: str,
-            **kwargs,
-    ):
-        self.template_field_ds = template_field_ds
-        self.template_field_ts = template_field_ts
-        self.data_interval_end = kwargs.pop("data_interval_end", "{{ data_interval_end }}")
-        super().__init__(application_file=application_file, **kwargs)
-
-    def execute(self, context: Context):
-        template = Template(self.application_file)
-        template_context = {
-            "ds": self.template_field_ds,
-            "ts": self.template_field_ts,
-            "data_interval_end": self.data_interval_end,
-        }
-        rendered_template = template.render(template_context)
-        self.application_file = rendered_template
-        logging.info(f"application file rendered is: \n{self.application_file}")
-        return super().execute(context)
-
 
 
 class SparkK8sJobBuilder(object):
---
+++ airflow_spark_on_k8s_job_builder/customizable_spark_k8s_operator.py
@@ -0,0 +1,112 @@
+
+from airflow.utils.context import Context
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from jinja2 import Template
+import logging
+from typing import Sequence
+
+
+class CustomizableSparkKubernetesOperator(SparkKubernetesOperator):
+    """
+    A decorator that allows using airflow macros inside spark k8s template
+    It does so by intercepting execute method with a sole purpose of rendering
+    a second time the jinja values of the SparkApplication yaml manifest
+
+    In case you need to add another macros to be jinja rendered in the
+    SparkApplication manifest - for example ts_nodash_with_tz - just follow the next steps:
+        a) add another constructor variable
+
+            ```python
+                def __init__(
+                        self,
+                        *,
+                        application_file: str,
+                        template_field_ds: str,
+                        template_field_ts: str,
+                        template_field_ts_nodash_with_tz: str
+                        **kwargs,
+                ):
+                self.template_field_YOUR_NEW_VAR = template_field_YOUR_NEW_VAR
+            ```
+
+        b) add that variable to the template fields (before this class constructor):
+
+            ```python
+                template_fields: Sequence[str] = ("application_file",
+                                                  "template_field_ds",
+                                                  "template_field_ts",
+                                                  "template_field_ts_nodash_with_tz")
+            ```
+
+
+        c) inject those macros when the builder class creates
+
+
+            ```python
+                task = CustomizableSparkKubernetesOperator(
+                    task_id=self._task_id,
+                    params=self._job_spec['params'],
+                    dag=self._dag,
+                    namespace=self._namespace,
+                    application_file=self._application_file,
+                    retries=self._retries,
+                    do_xcom_push=True,
+                    execution_timeout=self._task_timeout,
+                    template_field_ds='{{ ds }}',
+                    template_field_ts='{{ ts }}',
+                    template_field_ts_nodash_with_tz='{{ ts_nodash_with_tz }}'
+                )
+            ```
+
+        d) in `execute` method, add that variable to template_context:
+
+            ```python
+
+                def execute(self, context: Context):
+                    template = Template(self.application_file)
+                    template_context = {
+                        "ds": self.template_field_ds,
+                        "ts": self.template_field_ts,
+                        "ts_nodash_with_tz": self.template_field_ts_nodash_with_tz,
+                    }
+            ```
+
+
+    Ref docs:
+        - Airflow macros: https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
+
+    """
+
+    template_fields: Sequence[str] = (
+        "application_file",
+        "template_field_ds",
+        "template_field_ts",
+        "data_interval_end",
+    )
+
+    def __init__(
+            self,
+            *,
+            application_file: str,
+            template_field_ds: str,
+            template_field_ts: str,
+            **kwargs,
+    ):
+        self.template_field_ds = template_field_ds
+        self.template_field_ts = template_field_ts
+        self.data_interval_end = kwargs.pop("data_interval_end", "{{ data_interval_end }}")
+        super().__init__(application_file=application_file, **kwargs)
+
+    def execute(self, context: Context):
+        template = Template(self.application_file)
+        template_context = {
+            "ds": self.template_field_ds,
+            "ts": self.template_field_ts,
+            "data_interval_end": self.data_interval_end,
+        }
+        rendered_template = template.render(template_context)
+        self.application_file = rendered_template
+        logging.info(f"application file rendered is: \n{self.application_file}")
+        return super().execute(context)
+
+
